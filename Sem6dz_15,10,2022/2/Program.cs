0173afe145c1449ceaacc10c7aa81d429e31ac2a
D.Veselov <d.veselov@techmoney.ru>
2022-10-15 11:14:55 +0300

    Урок 1. Знакомство с языком программирования С#

---
+++ Sem6dz_15,10,2022/2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 > number2){
+    Console.WriteLine($"Число {number1} > {number2}");
+}else if (number1 < number2)
+{
+    Console.WriteLine($"Число {number1} < {number2}");
+}else{
+    Console.WriteLine($"Число {number1} = {number2}");
+}
