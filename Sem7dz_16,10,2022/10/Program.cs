2ea43528e871c012078e6fce52ad4004c11320fe
D.Veselov <d.veselov@techmoney.ru>
2022-10-16 10:43:42 +0300

    7dz

---
+++ Sem7dz_16,10,2022/10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number>99 && number<1000 || number<-99 && number>-1000){
+    number = number/10;
+    number = number%10;
+    number = Math.Abs(number);
+    Console.Write($"{number}");
+}else{
+    Console.Write("Некорректное число!");
+}
