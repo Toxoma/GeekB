dade25ba0a425067e3411f0a111c6e3e726fdd7c
D.Veselov <d.veselov@techmoney.ru>
2022-10-23 11:04:29 +0300

    9dz

---
+++ Sem9dz_23,10,2022/23/Program.cs
@@ -0,0 +1,50 @@
+﻿/* 
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+double GetConsole(string msg, bool notNull = false)
+{
+    double result;
+
+    while (true)
+    {
+        void LogWrong(string err)
+        {
+            Console.WriteLine(err);
+        }
+
+        Console.Write(msg);
+
+        string str = Console.ReadLine() ?? "";
+
+        if (double.TryParse(str, out double number))
+        {
+            if (notNull && number == 0)
+            {
+                LogWrong("Введите число != 0! Повторите ввод.\n");
+                continue;
+            }
+
+            result = number;
+            break;
+        }
+        else
+        {
+            LogWrong("Ввели некорректное число! Повторите ввод.\n");
+        }
+    }
+
+    return result;
+}
+
+void MakeDegree(double x, double y)
+{
+    Console.WriteLine(Math.Pow(x, y));
+}
+
+double x = GetConsole("Введите число != 0: ", true);
+double y = GetConsole("Введите степень: ");
+MakeDegree(x, y);
---
+++ Sem9dz_23,10,2022/29/Program.cs
@@ -0,0 +1,66 @@
+﻿/* 
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+int length = GetConsole("Введите длину массива: ");
+int max = GetConsole("Введите разброс значений в массиве [от 0; до x): ");
+
+int[] mas = new int[length];
+Random rnd = new Random();
+
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = rnd.Next(max);
+}
+
+Vivod(mas);
+
+void Vivod(int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (i == 0)
+        {
+            Console.Write($"[{mas[i]}, ");
+            continue;
+        }
+        if (i == length - 1)
+        {
+            Console.Write($"{mas[i]}]");
+            continue;
+        }
+        Console.Write($"{mas[i]}, ");
+    }
+}
+
+int GetConsole(string msg)
+{
+    int result;
+
+    while (true)
+    {
+        void LogWrong(string err)
+        {
+            Console.WriteLine(err);
+        }
+
+        Console.Write(msg);
+
+        string str = Console.ReadLine() ?? "";
+
+        if (int.TryParse(str, out int number) && number > 0)
+        {
+            result = number;
+            break;
+        }
+        else
+        {
+            LogWrong("Ввели некорректное число! Повторите ввод.\n");
+        }
+    }
+
+    return result;
+}
