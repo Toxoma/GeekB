3c20b6c4dc2b70f0e6c9db241d119358a6aaca91
D.Veselov <d.veselov@techmoney.ru>
2022-11-20 14:27:03 +0300

    dz13

---
+++ Sem13dz_20,11,2022/60/Program.cs
@@ -0,0 +1,97 @@
+﻿/* 
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+int GetConsole(string msg, int x)
+{
+    int result;
+
+    while (true)
+    {
+        void LogWrong(string err)
+        {
+            Console.WriteLine(err);
+        }
+
+        Console.Write(msg);
+
+        string str = Console.ReadLine() ?? "";
+
+        if (int.TryParse(str, out int number) && number > x)
+        {
+            result = number;
+            break;
+        }
+        else
+        {
+            LogWrong($"Нужно число > {x}.\n");
+        }
+    }
+
+    return result;
+}
+
+int[,,] InitArray(int m, int n, int o)
+{
+    Dictionary<int, int> znach = new Dictionary<int, int>();
+    Random rnd = new Random();
+    int NePovtor()
+    {
+        int result = 0;
+        while (true)
+        {
+            result = rnd.Next(10, 100);
+            if (!znach.ContainsKey(result))
+            {
+                znach.Add(result, 1);
+                return result;
+            }
+        }
+    }
+
+    int[,,] arr = new int[m, n, o];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < o; k++)
+            {
+                arr[i, j, k] = NePovtor();
+            }
+        }
+    }
+
+    return arr;
+}
+
+void Vivod(int[,,] arr)
+{
+    int m = arr.GetLength(0);
+    int n = arr.GetLength(1);
+    int o = arr.GetLength(2);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int z = 0; z < o; z++)
+            {
+                Console.WriteLine($"{arr[i, j, z]}({i},{j},{z})");
+            }
+        }
+    }
+}
+
+
+
+int[,,] arr = InitArray(
+    GetConsole("Введите кол-во строк: ", 0),
+    GetConsole("Введите кол-во столбцов: ", 0),
+    GetConsole("Введите глубину: ", 0)
+    );
+
+Vivod(arr);
---
+++ Sem13dz_20,11,2022/62/Program.cs
@@ -0,0 +1,123 @@
+﻿/* 
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+int GetConsole(string msg, int x)
+{
+    int result;
+
+    while (true)
+    {
+        void LogWrong(string err)
+        {
+            Console.WriteLine(err);
+        }
+
+        Console.Write(msg);
+
+        string str = Console.ReadLine() ?? "";
+
+        if (int.TryParse(str, out int number) && number > x)
+        {
+            result = number;
+            break;
+        }
+        else
+        {
+            LogWrong($"Нужно число > {x}.\n");
+        }
+    }
+
+    return result;
+}
+
+string[,] InitArray(int size)
+{
+    string[,] arr = new string[size, size];
+    int m = 0;
+    int n = 0;
+    int k = 1; //перебор присваиваемых значений
+    int maxCount = arr.GetLength(0) * arr.GetLength(1);
+    int side = 0; //0 - право, 1-низ, 2-лево, 3-верх
+
+    string GenerateValue()
+    {
+        string result = "";
+        if (Convert.ToString(k).Length < 2) result += "0" + k;
+        else result += k;
+        k++;
+        return result;
+    }
+
+    // stepBack = fasle - если в ячейке есть значение и необходимо продолжить отрисовку, а не вернутся на последнюю заполненную ячейку (иначе зацикливание происходит)
+    void InitValues(int i, int j, int _side, bool stepBack = true)
+    {
+        if (string.IsNullOrEmpty(arr[i, j]))
+        {
+            arr[i, j] = GenerateValue();
+            m = i;
+            n = j;
+        }
+        else if(stepBack)
+        {
+            if (k < maxCount)
+            {
+                side = _side;
+                InitValues(m, n, side, false);
+            }
+            return;
+        }
+
+        if (side == 0)
+        {
+            if (j < size - 1) InitValues(i, j + 1, 1);
+            else side = 1;
+        }
+        if (side == 1)
+        {
+            if (i < size - 1) InitValues(i + 1, j, 2);
+            else side = 2;
+        }
+        if (side == 2)
+        {
+            if (j > 0) InitValues(i, j - 1, 3);
+            else side = 3;
+        }
+        if (side == 3)
+        {
+            if (i > 0) InitValues(i - 1, j, 0);
+            else side = 0;
+        }
+    }
+    InitValues(m, n, side);
+
+    return arr;
+}
+
+void Vivod(string[,] arr)
+{
+    int m = arr.GetLength(0);
+    int n = arr.GetLength(1);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j != n - 1)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            else
+            {
+                Console.Write($"{arr[i, j]}\n");
+            }
+        }
+    }
+}
+
+string[,] arr = InitArray(GetConsole("Введите размерность массива: ", 1));
+Vivod(arr);
