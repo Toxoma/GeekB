6c09201c1b0fa09553bb22c2d1f9107ffb97635e
D.Veselov <d.veselov@techmoney.ru>
2022-11-26 11:04:43 +0300

    dz14

---
+++ Sem14dz_26,11,2022/64/Program.cs
@@ -0,0 +1,60 @@
+﻿/* 
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int GetConsole(string msg)
+{
+    int result;
+
+    while (true)
+    {
+        void LogWrong(string err)
+        {
+            Console.WriteLine(err);
+        }
+
+        Console.Write(msg);
+
+        string str = Console.ReadLine() ?? "";
+
+        if (int.TryParse(str, out int number) && number>0)
+        {
+            result = number;
+            break;
+        }
+        else
+        {
+            LogWrong($"Нужно натуральное число.\n");
+        }
+    }
+
+    return result;
+}
+
+// тут немного излишне т.к. не увидел что нужны ток натуральные
+void Vivod(int x)
+{
+    void Rekursia(int y)
+    {
+        if(y == 1){
+            Console.Write($"{y}\"");
+            return;
+        }
+        Console.Write($"{y}, ");
+        if (y > 1)
+            y--;
+        else
+            y++;
+
+        Rekursia(y);
+    }
+
+    Console.Write($"N = {x} -> \"");
+    Rekursia(x);
+}
+
+int x = GetConsole("Введите число: ");
+Vivod(x);
---
+++ Sem14dz_26,11,2022/66/Program.cs
@@ -0,0 +1,77 @@
+﻿/* 
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetConsole(string msg, int x = -1)
+{
+    int result;
+
+    while (true)
+    {
+        void LogWrong(string err)
+        {
+            Console.WriteLine(err);
+        }
+
+        Console.Write(msg);
+
+        string str = Console.ReadLine() ?? "";
+
+        if (int.TryParse(str, out int number) && number > 0)
+        {
+            if (x != -1)
+            {
+                if (number != x)
+                {
+                    result = number;
+                    break;
+                }
+                else
+                {
+                    LogWrong($"Нужно натуральное число не совпадающее с {x}.\n");
+                }
+            }
+            else
+            {
+                result = number;
+                break;
+            }
+        }
+        else
+        {
+            LogWrong($"Нужно натуральное число.\n");
+        }
+    }
+
+    return result;
+}
+
+int GetSumm(int m, int n)
+{
+    int result = 0;
+
+    void Rekursia(int m, int n)
+    {
+        result += m;
+
+        if (m > n)
+            m--;
+        else if (m < n)
+            m++;
+        else
+            return;
+
+        Rekursia(m, n);
+    }
+    Rekursia(m, n);
+
+    return result;
+}
+
+int m = GetConsole("Введите число m: ");
+int n = GetConsole("Введите число n: ", m);
+int sum = GetSumm(m, n);
+Console.WriteLine($"M = {m}; N = {n} -> {sum}");
