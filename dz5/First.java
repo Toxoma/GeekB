2870c4ff11104d94f3ea87b7f9c227ed8211dd1f
D.Veselov <d.veselov@techmoney.ru>
2023-02-27 17:38:12 +0300

    dz6

--- dz5/First.java
+++ dz5/First.java
@@ -7,7 +7,12 @@ public class First {
     static String cell = "   ";
     static String start = " @ ";
     static String stop = " x ";
-    static String point = " o ";
+    static String point = " ° ";
+
+    static int curX = 1;
+    static int curY = 1;
+
+    static boolean continueFlag = true;
 
     /*
      * 0 - клетка
@@ -30,7 +35,101 @@ public class First {
     };
 
     public static void main(String[] args) {
+        First.findWay(maze);
         First.vivod(maze);
+    }
+
+    public static void makeStep() {
+        First.vivod(maze);
+        int tempX = curX;
+        int tempY = curY;
+
+        for (int i = 0; i < 4; i++) {
+            if (First.isEnd())
+                break;
+            switch (i) {
+                case 0:
+                    First.goTop();
+                    curX = tempX;
+                    curY = tempY;
+                    break;
+                case 1:
+                    First.goLeft();
+                    curX = tempX;
+                    curY = tempY;
+                    break;
+                case 2:
+                    First.goDown();
+                    curX = tempX;
+                    curY = tempY;
+                    break;
+                case 3:
+                    First.goRight();
+                    curX = tempX;
+                    curY = tempY;
+                    break;
+            }
+        }
+
+        if (First.isEnd())
+            return;
+        maze[curX][curY] = cell;
+    }
+
+    public static boolean isEnd() {
+        if (!continueFlag || maze[curX][curY] == stop) {
+            continueFlag = false;
+            return true;
+        }
+        return false;
+    }
+
+    public static void goTop() {
+        curX--;
+        if (First.isEnd())
+            return;
+        if (curX > 0 && maze[curX][curY] == cell) {
+            maze[curX][curY] = point;
+            First.makeStep();
+        } else
+            curX++;
+    }
+
+    public static void goRight() {
+        curY++;
+        if (First.isEnd())
+            return;
+        if (curY < maze.length - 1 && maze[curX][curY] == cell) {
+            maze[curX][curY] = point;
+            First.makeStep();
+        } else
+            curY--;
+    }
+
+    public static void goDown() {
+        curX++;
+        if (First.isEnd())
+            return;
+        if (curX < maze[0].length - 1 && maze[curX][curY] == cell) {
+            maze[curX][curY] = point;
+            First.makeStep();
+        } else
+            curX--;
+    }
+
+    public static void goLeft() {
+        curY--;
+        if (First.isEnd())
+            return;
+        if (curY > 0 && maze[curX][curY] == cell) {
+            maze[curX][curY] = point;
+            First.makeStep();
+        } else
+            curY++;
+    }
+
+    public static void findWay(String[][] mas) {
+        First.makeStep();
     }
 
     public static void vivod(String[][] mas) {
