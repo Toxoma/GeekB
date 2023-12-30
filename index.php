d068d99d2a788a8a826d15a48c8bc400ca69349f
D.Veselov <d.veselov@techmoney.ru>
2023-12-30 13:45:43 +0300

    Основы PHP

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+//echo "Привет, GeekBrains!<br>".date("Y-m-d H:i:s") ."<br><br>";
+
+//echo "Что-то еще 123456!!!!!";
+
+
+$a = 1;
+$b = 2;
+
+$a+=+$b-$b=$a;
+var_dump($a);
+var_dump($b);
+
+//phpinfo();
