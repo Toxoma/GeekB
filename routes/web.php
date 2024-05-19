55f1d0a0f193d914622d8eb2c58f80e67cc66247
Toxoma <daniil.veselov2012@yandex.ru>
2024-05-19 20:12:47 +0300

    Урок 4. Работа с шаблонами. Шаблонизатор Blade

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,25 @@
 use App\Models\Employee;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/userform', [\App\Http\Controllers\FormProcessor::class, 'index']);
 Route::post('/store_form', [\App\Http\Controllers\FormProcessor::class, 'store']);
 Route::get('/test_database', function () {
     $employee = new Employee();
     $employee->save();
+});
+Route::get('/', function () {
+    return view('home', [
+        'name'=>'name',
+        'age'=>15,
+        'position'=>'position',
+        'address'=>'address',
+    ]);
+});
+Route::get('/contacts', function () {
+    return view('contacts', [
+        'address'=>'address',
+        'post_code'=>'post_code',
+        'email'=>'email',
+        'phone'=>'phone',
+    ]);
 });
