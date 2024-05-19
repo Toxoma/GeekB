66edf63f72c9a21ec42dee0c09a7dc1e30ec62dc
Toxoma <daniil.veselov2012@yandex.ru>
2024-05-19 18:23:35 +0300

    Урок 3. Работа с базами данных. ORM-система Eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employee;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,7 @@ Route::get('/', function () {
 
 Route::get('/userform', [\App\Http\Controllers\FormProcessor::class, 'index']);
 Route::post('/store_form', [\App\Http\Controllers\FormProcessor::class, 'store']);
+Route::get('/test_database', function () {
+    $employee = new Employee();
+    $employee->save();
+});
