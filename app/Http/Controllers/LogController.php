8b724928593cb7bba1f22456fb310424113c9243
Toxoma <daniil.veselov2012@yandex.ru>
2024-06-17 16:12:24 +0300

    Урок 8

---
+++ app/Http/Controllers/LogController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LogController extends Controller
+{
+    public function index()
+    {
+        $logs = DB::table("logs")->paginate(15);
+        return view('logs', ['logs' => $logs]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@ Route::post('/store-user', [\App\Http\Controllers\UserController::class, 'store'
 Route::get('/user', [\App\Http\Controllers\UserController::class, 'index']);
 Route::get('/user/{id}', [\App\Http\Controllers\UserController::class, 'get']);
 Route::get('/resume/{id}', [\App\Http\Controllers\PdfGeneratorController::class, 'index']);
+Route::get('/logs', [\App\Http\Controllers\LogController::class, 'index']);
+
