8273f669f0edf74c958dd49808de935b8e1e12af
Toxoma <daniil.veselov2012@yandex.ru>
2024-06-17 16:59:43 +0300

    Урок 9

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Events\NewsHidden;
+use App\Models\News;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -14,4 +16,19 @@ Route::get('/user', [\App\Http\Controllers\UserController::class, 'index']);
 Route::get('/user/{id}', [\App\Http\Controllers\UserController::class, 'get']);
 Route::get('/resume/{id}', [\App\Http\Controllers\PdfGeneratorController::class, 'index']);
 Route::get('/logs', [\App\Http\Controllers\LogController::class, 'index']);
+
+Route::get('/news/create-test', function (){
+    $news = new News();
+    $news->title = 'Test news title';
+    $news->body = 'Test news body';
+    $news->save();
+    return $news;
+});
+Route::get('/news/{id}/hide', function ($id){
+    $news = News::findOrFail($id);
+    $news->hidden = true;
+    $news->save();
+    NewsHidden::dispatch($news);
+    return 'News hidden';
+});
 
